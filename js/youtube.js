289170c30fcf1f0de3d4fab158820c872282be9f
carrottechplus <blohzzang@gmail.com>
2023-06-07 14:22:04 +0900

    youtube api 연동 script 작업

--- js/youtube.js
+++ js/youtube.js
@@ -1,32 +1,54 @@
-// 나의 구글 api key값
-const key = 'AIzaSyC4TpEbx2d9lOtjiVQIg3b6wA6ZKKrDL7c';
-// 나의 youtube playlist url (공개로 설정되어야 함)
-const list = 'PLQZTVbf9_qAnD8qhXt02stKVXVQd-v-pf';
-// 한페이지에 몇개 데이터를 가져올지 (maxResults)
-const num = 10;
-// youtube data api 사용법에 따라 작성
-const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
-
 const wrap = document.querySelector('.youtube .wrap');
+fetchData();
 
-fetch(url)
-	.then((data) => data.json())
-	.then((json) => {
-		// console.log(json);
-		// console.log(json.items); // 배열
-		let tags = '';
+wrap.addEventListener('click', (e) => {
+	console.dir(e.target);
+	if (e.target.nodeName !== 'IMG') return;
+	console.log(e.target.getAttribute('alt'));
+});
 
-		json.items.forEach((item) => {
-			//반복되는 객체 안 spippet
-			tags += `
+// 분리해보기
+// 데이터 fetching함수
+async function fetchData() {
+	const key = 'AIzaSyC4TpEbx2d9lOtjiVQIg3b6wA6ZKKrDL7c';
+	const list = 'PLQZTVbf9_qAnD8qhXt02stKVXVQd-v-pf';
+	const num = 10;
+	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
+
+	const data = await fetch(url);
+	const json = await data.json();
+	console.log(json.items);
+
+	createList(json.items);
+
+	// fetch(url)
+	// 	.then((data) => data.json())
+	// 	.then((json) => {});
+}
+
+// 동적으로 목록을 만드는 함수
+
+function createList(arr) {
+	let tags = '';
+	arr.forEach((item) => {
+		let title = item.snippet.title;
+		let desc = item.snippet.description;
+		let date = item.snippet.publishedAt;
+		tags += `
 			<article>
-				<h2>${item.snippet.title}</h2>
-				<img src=${item.snippet.thumbnails.standard.url}>
-				<p>${item.snippet.description}</p>
-				<span>${item.snippet.publishedAt}</span>
+				<h2>${title.length > 50 ? title.substr(0, 50) + '...' : title}</h2>
+				<div class='txt'>
+					<p>${desc.length > 200 ? desc.substr(0, 200) + '...' : desc}</p>
+					<span>${date.split('T')[0].split('-').join('/')}</span>
+				</div>
+				<div class='pic'>
+					<img src=${item.snippet.thumbnails.standard.url} alt=${item.snippet.resourceId.videoId}>
+				</div>
 			</article>
 			`;
-		});
-
-		wrap.innerHTML = tags;
 	});
+
+	wrap.innerHTML = tags;
+}
+
+// 이벤트가 동작되는 함수
