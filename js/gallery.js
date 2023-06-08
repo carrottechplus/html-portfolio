15b2648a25fb6fc632983fdb2465d9832815b89e
carrottechplus <blohzzang@gmail.com>
2023-06-08 12:17:53 +0900

    isotope 플러그인 연동

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,9 @@
 const wrap = document.querySelector('.gallery .wrap');
 const api_key = '6c70577e2661042cd0ab587b17f6c944';
 const method_interest = 'flickr.interestingness.getList';
-const num = 50;
+const num = 20;
 const baseUrl = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&format=json&nojsoncallback=1&per_page=${num}`;
+// 검색량 많은 순 사진 불러옴
 
 fetch(baseUrl)
 	.then((res) => res.json())
@@ -14,13 +15,22 @@ fetch(baseUrl)
 
 		items.forEach((item) => {
 			tags += `
-      <article>
-        <div class='pic'>
-          <img src='https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg' />
+      <li class='item'>
+        <div class=''>
+          <a href='https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg'>
+            <img src='https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg' />
+          </a>
+          <p>${item.title === '' ? 'Have a good day !' : item.title}</p>
         </div>
-      </article>
+      </li>
       `;
 		});
 
 		wrap.innerHTML = tags;
+
+		new Isotope(wrap, {
+			// options
+			itemSelector: '.item',
+			transitionDuration: '0.5s',
+		});
 	});
