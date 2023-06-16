c82b2a757df8ec306344b84fa07fc06a0067c92c
carrottechplus <blohzzang@gmail.com>
2023-06-16 12:20:35 +0900

    카카오맵 api 마커에 원하는 이미지 테스트

--- js/location.js
+++ js/location.js
@@ -1,8 +1,15 @@
 const mapContainer = document.querySelector('#map');
-const position = new kakao.maps.LatLng(33.450701, 126.570667); //지도 위치 인스턴스
-const mapOption = { center: position, level: 3 }; // 지도 생성 옵션
+const position = new kakao.maps.LatLng(37.503598118960525, 126.97550644009758); //지도 위치 인스턴스
+const mapOption = { center: position, level: 2 }; // 지도 생성 옵션
 const map = new kakao.maps.Map(mapContainer, mapOption); // 지도 인스턴스 생성
-const marker = new kakao.maps.Marker({ position: position }); // 마커 인스턴트 생성
+
+const imgSrc = 'img/marker1.png';
+const imgSize = new kakao.maps.Size(232, 99);
+const imgOpt = { offset: new kakao.maps.Point(116, 99) };
+
+const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOpt);
+
+const marker = new kakao.maps.Marker({ position: position, image: markerImage }); // 마커 인스턴트 생성
 
 // 마커 인스턴스에 setMap함수로 지도 인스턴스 바인딩
 marker.setMap(map);
