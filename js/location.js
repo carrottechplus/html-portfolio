d284217c788c44a3c71d872d3fa40ae8d4f5a8de
carrottechplus <blohzzang@gmail.com>
2023-06-18 16:24:13 +0900

    카카오 맵 api 좌표 여러개

--- js/location.js
+++ js/location.js
@@ -1,15 +1,43 @@
 const mapContainer = document.querySelector('#map');
-const position = new kakao.maps.LatLng(37.503598118960525, 126.97550644009758); //지도 위치 인스턴스
-const mapOption = { center: position, level: 2 }; // 지도 생성 옵션
-const map = new kakao.maps.Map(mapContainer, mapOption); // 지도 인스턴스 생성
+const btns = document.querySelectorAll('.branch li');
 
-const imgSrc = 'img/marker1.png';
-const imgSize = new kakao.maps.Size(232, 99);
-const imgOpt = { offset: new kakao.maps.Point(116, 99) };
+const markerInfo = [
+	{
+		title: '광화문',
+		position: new kakao.maps.LatLng(37.57598923870742, 126.9768610125929),
+		imgSrc: 'img/marker1.png',
+		imgSize: new kakao.maps.Size(232, 99),
+		imgPos: { offset: new kakao.maps.Point(116, 99) },
+		button: btns[0],
+	},
+	{
+		title: '수정전',
+		position: new kakao.maps.LatLng(37.578987, 126.976045),
+		imgSrc: 'img/marker2.png',
+		imgSize: new kakao.maps.Size(232, 99),
+		imgPos: { offset: new kakao.maps.Point(116, 99) },
+		button: btns[1],
+	},
+	{
+		title: '카카오본사',
+		position: new kakao.maps.LatLng(33.450701, 126.570667),
+		imgSrc: 'img/marker1.png',
+		imgSize: new kakao.maps.Size(232, 99),
+		imgPos: { offset: new kakao.maps.Point(116, 99) },
+		button: btns[2],
+	},
+];
 
-const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOpt);
+//markerInfo의 첫번째 데이터로 기본 지도 인스턴스 생성
+const map = new kakao.maps.Map(mapContainer, { center: markerInfo[0].position, level: 3 });
 
-const marker = new kakao.maps.Marker({ position: position, image: markerImage }); // 마커 인스턴트 생성
+// markerInfo 배열 데이터를 통해서 기존 맵인스턴스에 반복을 돌며 마커 생성하고 바인딩
+markerInfo.forEach((info) => {
+	const marker = new kakao.maps.Marker({ position: info.position, image: new kakao.maps.MarkerImage(info.imgSrc, info.imgSize, info.imgPos) });
 
-// 마커 인스턴스에 setMap함수로 지도 인스턴스 바인딩
-marker.setMap(map);
+	marker.setMap(map);
+
+	info.button.addEventListener('click', () => {
+		map.panTo(info.position);
+	});
+});
